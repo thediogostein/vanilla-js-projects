3661b34d779ff723bc71ea851958f7b7f95dabc0
bruxo <diogo.stein@outlook.com.br>
2023-05-24 17:14:33 -0300

    Add shuffle function

--- Show a fortune/main.js
+++ Show a fortune/main.js
@@ -1,8 +1,53 @@
+// The fortune responses
+let responses = [
+  'It is certain.',
+  'It is decidedly so.',
+  'Without a doubt.',
+  'Yes - definitely.',
+  'You may rely on it.',
+  'As I see it, yes.',
+  'Most likely.',
+  'Outlook good.',
+  'Yes.',
+  'Signs point to yes.',
+  'Reply hazy, try again.',
+  'Ask again later.',
+  'Better not tell you now.',
+  'Cannot predict now.',
+  'Concentrate and ask again.',
+  "Don't count on it.",
+  'My reply is no.',
+  'My sources say no.',
+  'Outlook not so good.',
+  'Very doubtful.',
+];
+
 const btn = document.querySelector('button');
 const fortune = document.querySelector('#fortune');
 
-const getFortune = () => {
-  fortune.textContent = "You're going to have a wonderful day!";
-};
+function shuffle(array) {
+  let currentIndex = array.length;
+  let temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Get the fortune and render it into the DOM
+function getFortune() {
+  shuffle(responses);
+  fortune.textContent = responses[0];
+}
 
 btn.addEventListener('click', getFortune);
